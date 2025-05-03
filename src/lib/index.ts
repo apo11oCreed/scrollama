6fc403ea557fa2db03592e0289c825bc9707686a
Ubuntu <ubuntu@ip-172-31-84-252.ec2.internal>
2025-05-03 21:40:09 +0000

    chore(configs): add central module and component list

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,4 @@
 // Reexport your entry components here
+export { default as Graphic } from "./components/Graphic.svelte";
+export { default as Text } from "./components/Text.svelte";
+export { default as Step } from "./components/Step.svelte";
