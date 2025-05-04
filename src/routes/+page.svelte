6c2d0d666e00d69e0c25580b4997baa9c6cd0729
Ubuntu <ubuntu@ip-172-31-84-252.ec2.internal>
2025-05-04 20:36:36 +0000

    chore(config): add d3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,26 @@
 <script>
     import { Graphic, Text } from '$lib';
+    import * as d3 from 'd3';
+    import { onMount } from 'svelte';
+    // using d3 for convenience, and storing a selected elements
+    
+    onMount(()=>{
+        const _container = d3.select('#scroll');
+        const _graphic = _container.select('.scroll__graphic');
+        const _chart = _graphic.select('.chart');
+        const _text = _container.select('.scroll__text');
+        const _step = _text.selectAll('.step');
+        
+        console.log(_container);
+    });
+    
 </script>
 <section id="scroll">
     <Graphic />
     <Text />
 </section>
 <style lang="stylus">
+#scroll
+    position relative
 </style>
+<!--https://pudding.cool/process/introducing-scrollama/-->
