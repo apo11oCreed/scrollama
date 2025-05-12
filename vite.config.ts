54beafb08268777b71df2cc5e50aab9d10312cfa
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-05-12 09:27:03 +0000

    chore(config): add new entry to host list

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 	    port: '8080',
-	    allowedHosts: ['6240c6c47dce467a9c50d432d7f3f47b.vfs.cloud9.us-east-1.amazonaws.com']
+	    allowedHosts: ['6240c6c47dce467a9c50d432d7f3f47b.vfs.cloud9.us-east-1.amazonaws.com','47f0519e5afd4ba28b322f42a9a35d1d.vfs.cloud9.us-east-1.amazonaws.com']
 	}
 });
