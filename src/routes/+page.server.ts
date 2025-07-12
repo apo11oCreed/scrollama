29e3ab424c153c9c036098304f883bf16da21657
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-07-12 19:29:56 +0000

    config(load): update forecastReq to return only period and temperature objects

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,12 +3,28 @@ import graphicsData from '$lib/data/graphics.json';
 
 export const load: PageServerLoad = async ({ fetch, setHeaders }) => {
   
-  const forecastReq = await fetch('https://api.weather.gov/gridpoints/BOX/42,71/forecast');
-  setHeaders({
-    'User-Agent': 'nelson.ncdesigns@gmail.com ncdesigns-studio.com'
-  });
+  const forecastReq = await fetch(
+    'https://api.weather.gov/gridpoints/BOX/42,71/forecast',
+    {
+      headers :{
+        'User-Agent': 'nelson.ncdesigns@gmail.com ncdesigns-studio.com'
+      }
+    });
   
-  const forecastData = await forecastReq.json();
+  const forecastData = await forecastReq.json()
+  .then(data=>{
+    
+    const periods = data.properties.periods;
+    
+    let tempObjs = [];
+    
+    for(let period in periods){
+      tempObjs.push({period:periods[period].name,temperature:periods[period].temperature});
+    }
+    
+    return tempObjs;
+    
+  });
     
   return { 
     forecastData,
