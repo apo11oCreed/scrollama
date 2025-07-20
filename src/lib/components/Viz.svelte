8be6bbfc813a0edeb17da089535481d0536ba21e
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-07-20 18:27:44 +0000

    feat(d3): bind d3 container and add initial data configs

--- src/lib/components/Viz.svelte
+++ src/lib/components/Viz.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import { page } from '$app/stores';
   import { get } from 'svelte/store';
+  import * as d3 from 'd3';
   
   const { data } = get(page); // get the page store synchronously
   const { forecastData } = data;
-  console.log('Forecast data for Boston: ', forecastData.properties.periods);
+  console.log('Forecast data for Boston: ', forecastData);
+  
+  let graph;
+  let container;
+  
+  onMount(()=>{
+    // Select the body element as the parent container
+    const svgContainer = d3.select(container)
+      .append("svg") // Add a div to hold the SVGs
+      .attr("class", "svg-container");
+  
+    svgContainer.selectAll('.item')
+    .data(forecastData)
+    .enter()
+    .append('g')
+    .text(function(d){
+      console.log(d.period);
+      return d.period;
+    });
+  });
   
 </script>
 <p>This is a placeholder for the data visualization component</p>
+<div bind:this={container}></div>
 <style lang="stylus">
     
 </style>
